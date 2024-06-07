3902c38eaba8f3cde3b0b02d3f4d11ffbcb86e1d
Lourdes <lourdeswairimu@gmail.com>
2024-06-07 13:52:37 +0300

    css

--- house_hunter/settings.py
+++ house_hunter/settings.py
@@ -132,6 +132,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'main', 'static'),
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
